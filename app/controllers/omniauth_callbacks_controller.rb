b0e394ddcc858b60dca7d68da0da555cf6d23901
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2016-05-18 17:10:17 -0300

    Automatically confirm users signed in via oauth2-moodle
    Closes #1

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,9 +3,15 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      if !@user.confirmed?
+        # automatically confirm users
+        @user.skip_confirmation_notification!
+        @user.confirm
+      end
+
+      sign_in_and_redirect @user, :event => :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Moodle") if is_navigational_format?
-    elsif 
+    elsif
       redirect_to new_user_registration_url
     end
   end
